37b2a7aaa8dd95e9b6a2b8f2d573455f40e21f39
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-10 02:27:02 +0600

    last

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py flush --no-input
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-# Use verbosity 3 so we can see exactly what happens in the logs
-python manage.py loaddata local_data.json --verbosity 3
+
+# This line loads your local data automatically during deployment
+python manage.py loaddata local_data.json
