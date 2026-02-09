c778d1aa7699fc0282e911b4cf98c8c998ba6ee5
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-10 02:48:30 +0600

    new file

--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-
-# This line loads your local data automatically during deployment
-python manage.py loaddata local_data.json
