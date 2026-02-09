171ef085073dcffa005d28720764c0a4c458da73
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-10 02:13:14 +0600

    new

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
+python manage.py flush --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Create the superuser automatically using the env vars above
-# We use || true so the build doesn't fail if the user already exists
-python manage.py createsuperuser --noinput || true
+# Use verbosity 3 so we can see exactly what happens in the logs
+python manage.py loaddata local_data.json --verbosity 3
