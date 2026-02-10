0bbbb9d6062aaf1afc4c8975c89588088e54971d
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-10 14:12:12 +0600

    fix signals

--- tasks/signals.py
+++ tasks/signals.py
@@ -4,23 +4,26 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 #new part start 
 #signals
-@receiver(m2m_changed,sender=Task.assigned_to.through)
-def notify_employee_on_task_creation(sender,instance,action,**kwargs):
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def notify_employee_on_task_creation(sender, instance, action, **kwargs):
     if action == 'post_add':
-        assigned_emails=[emp.email for emp in instance.assigned_to.all()]
-       
+        assigned_emails = [emp.email for emp in instance.assigned_to.all() if emp.email]
+        
+        if assigned_emails: # Only try to send if there are recipients
+            send_mail(
+                'New task assigned',
+                f'You have been assigned to the task: {instance.title}',
+                'mdarmanislam20021@gmail.com',
+                assigned_emails,
+                fail_silently=True, # Recommended for production
+            )
 
-        send_mail(
-            'New task assigned',
-            f'You have been assigned to the task: {instance.title}',
-            'mdarmanislam20021@gmail.com',        # From
-            assigned_emails,        # To
             
-)
-
-@receiver(post_delete,sender=Task)
-def delete_assosiate_details(sender,instance,**kwargs):
-    if instance.details:
-        print(instance)
+@receiver(post_delete, sender=Task)
+def delete_assosiate_details(sender, instance, **kwargs):
+    # hasattr safely checks if the 'details' relation exists
+    if hasattr(instance, 'details'):
         instance.details.delete()
-        print("deleted successfully")
+        print(f"Details for {instance.title} deleted successfully")
+    else:
+        print(f"No details found for {instance.title}, skipping deletion")
