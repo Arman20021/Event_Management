1348c73c7450270f4ea08339a91550bf83800933
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-10 14:27:12 +0600

    signals

--- users/signals.py
+++ users/signals.py
@@ -5,19 +5,45 @@ from django.contrib.auth.models import User,Group
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 
-@receiver(post_save,sender=User)
-def send_activation_email(sender,instance,created,**kwargs):
-    if created:
-        token=default_token_generator.make_token(instance)
-        activation_url=f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
+# @receiver(post_save,sender=User)
+# def send_activation_email(sender,instance,created,**kwargs):
+#     if created:
+#         token=default_token_generator.make_token(instance)
+#         activation_url=f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
 
-        subject='Acitvate Your account'
-        message=f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}\n\nThank You'
-        recipient_list=[instance.email]
+#         subject='Acitvate Your account'
+#         message=f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}\n\nThank You'
+#         recipient_list=[instance.email]
+#         try:
+#             send_mail(subject,message,settings.EMAIL_HOST_USER,recipient_list)
+#         except Exception as e:
+#             print(f'Failed to send email {instance.email}: {str(e)}')
+
+@receiver(post_save, sender=User)
+def send_activation_email(sender, instance, created, **kwargs):
+    if created:
+        token = default_token_generator.make_token(instance)
+        activation_url = f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
+
+        subject = 'Activate Your Account'
+        message = f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}'
+        recipient_list = [instance.email]
+        
         try:
-            send_mail(subject,message,settings.EMAIL_HOST_USER,recipient_list)
+            # Added fail_silently=False so we can catch the exception, 
+            # but you can set it to True to prevent hanging.
+            send_mail(
+                subject, 
+                message, 
+                settings.EMAIL_HOST_USER, 
+                recipient_list, 
+                fail_silently=False 
+            )
         except Exception as e:
-            print(f'Failed to send email {instance.email}: {str(e)}')
+            # This logs the error to the Render console instead of crashing the site
+            print(f'EMAIL ERROR: {str(e)}')
+
+
 
 @receiver(post_save, sender=User)
 def assign_role(sender, instance, created, **kwargs):
