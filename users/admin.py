dcd3b834be21b1879671952f35a0f54a313ab7c4
Arman Islam <mdarmanislam20021@gmail.com>
2026-02-17 22:56:41 +0600

    bug fix

--- users/admin.py
+++ users/admin.py
@@ -1,26 +1,23 @@
 from django.contrib import admin
 from users.models import CustomUser
 from django.contrib.auth.admin import UserAdmin
-# Register your models here.
-
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('first_name',
-         'last_name', 'email', 'bio', 'profile_image')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff',
-         'is_superuser', 'groups', 'user_permissions')}),
-        ('Importants Dates', {'fields': ('last_login', 'date_joined')})
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'bio', 'profile_image')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide'),
-            'fields': ('username', 'password1', 'password2', 'email', 'bio', 'profile_image')
-        })
+            'classes': ('wide',),   
+            'fields': ('username', 'password1', 'password2', 'email', 'bio', 'profile_image', 'is_staff', 'is_active')  # ✅ include required fields
+        }),
     )
 
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
